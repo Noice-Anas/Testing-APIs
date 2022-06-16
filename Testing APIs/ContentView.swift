db770546ae0562894d4e6e1fc75ced071dd0339f
Noice_Anas <anas.alhalabi2002@gmail.com>
2022-06-16 15:50:40 +0300

    Initial Commit

---
+++ Testing APIs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testing APIs
+//
+//  Created by Noice_anas on 16/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testing APIs/Testing_APIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_APIsApp.swift
+//  Testing APIs
+//
+//  Created by Noice_anas on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Testing_APIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
