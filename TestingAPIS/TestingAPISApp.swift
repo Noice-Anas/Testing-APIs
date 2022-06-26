e4f826288780cd1f2a5e577d984b7602e5736efd
Noice_Anas <anas.alhalabi2002@gmail.com>
2022-06-26 12:40:32 +0300

    Initial Commit

---
+++ TestingAPIS/TestingAPISApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAPISApp.swift
+//  TestingAPIS
+//
+//  Created by Noice_anas on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestingAPISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
